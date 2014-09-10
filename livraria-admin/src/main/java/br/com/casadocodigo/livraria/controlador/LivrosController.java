3124810b14e3ddcc2912b7a5da405f04f06d9715
Andressa Albuquerque <albuquerque.ar@gmail.com>
2014-09-09 22:57:45 -0300

    Brincando com Validator

--- livraria-admin/src/main/java/br/com/casadocodigo/livraria/controlador/LivrosController.java
+++ livraria-admin/src/main/java/br/com/casadocodigo/livraria/controlador/LivrosController.java
@@ -2,8 +2,12 @@ package br.com.casadocodigo.livraria.controlador;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.validator.SimpleMessage;
+import br.com.caelum.vraptor.validator.Validator;
 import br.com.casadocodigo.livraria.modelo.Estante;
 import br.com.casadocodigo.livraria.modelo.Livro;
 import br.com.casadocodigo.livraria.persistencia.UmaEstanteQualquer;
@@ -11,12 +15,41 @@ import br.com.casadocodigo.livraria.persistencia.UmaEstanteQualquer;
 @Controller
 public class LivrosController {
 
+	private Estante estante;
+	private Result result;
+	private Validator validator;
+
+	@Inject
+	public LivrosController(Estante estante, Result result, Validator validator) {
+		this.estante = estante;
+		this.result = result;
+		this.validator = validator;
+	}
+
+	/**
+	 * @deprecated Apenas para o CDI.
+	 */
+	LivrosController() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public void formulario() {
 
 	}
 
-	public void salva(Livro livro, Result result) {
+	public void salva(Livro livro) {
 		Estante estante = new UmaEstanteQualquer();
+
+		if (livro.getTitulo() == null) {
+			validator.add(new SimpleMessage("titulo", "Título é obrigatório!"));
+		}
+
+		if (livro.getPreco() == null) {
+			validator.add(new SimpleMessage("preco", "Preço é obrigatório!"));
+		}
+		
+		validator.onErrorRedirectTo(this).formulario();
+
 		estante.guarda(livro);
 
 		result.include("mensagem", "Livro salvo com sucesso!");
@@ -28,9 +61,7 @@ public class LivrosController {
 		return estante.todosOsLivros();
 	}
 
-	public void edita(String isbn, Result result) {
-		Estante estante = new UmaEstanteQualquer();
-
+	public void edita(String isbn) {
 		Livro livroEncontrado = estante.buscaPorIsbn(isbn);
 
 		if (livroEncontrado == null) {
@@ -39,7 +70,7 @@ public class LivrosController {
 			result.include(livroEncontrado);
 			result.of(this).formulario();
 		}
-		
+
 	}
 
 }
--- livraria-admin/src/main/webapp/WEB-INF/jsp/livros/formulario.jsp
+++ livraria-admin/src/main/webapp/WEB-INF/jsp/livros/formulario.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
@@ -7,6 +8,18 @@
 		<title>Formulário Livraria</title>
 	</head>
 	<body>
+	
+		<ul class="errors">
+			
+			<c:forEach items="${errors}" var="error">
+				<li> 
+					${error.category}: 
+					${error.message}
+				</li>
+			</c:forEach>
+		
+		</ul>
+	
 		<form method="post" action="${linkTo[LivrosController].salva}">
 		
 			<input type="hidden" name="livro.id" value="${livro.id}" />
